c368d2e09662738685ff4834cdceb844b993231f
Scott Ruzal <hi@scottruzal.com>
2017-07-12 13:14:34 -0400

    Lovus Theme Install

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2017, 12-1, 25, 10)}); // year, month, date, hour
+        });
+});		
+
