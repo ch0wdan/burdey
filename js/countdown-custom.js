67d4f8c372cfe50dfe67a0c13b17683fac0bbea4
Scott Ruzal <hi@scottruzal.com>
2017-09-19 19:33:16 -0400

    More Content Updates

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2018, 8-31, 25, 10)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2018, 08-1, 31, 18)}); // year, month, date, hour
         });
 });
