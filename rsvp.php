88c8d9c67457deec5bc168b3e43ce9b0f7de2852
Scott Ruzal <hi@scottruzal.com>
2017-09-22 20:31:14 -0400

    Fixes

--- rsvp.php
+++ rsvp.php
@@ -1,6 +1,6 @@
 <?php
-$subject = 'You Got Message'; // Subject of your email
-$to = 'info@designesia.com';  //Recipient's E-mail
+$subject = 'You Got A Message on Sceather.com'; // Subject of your email
+$to = 'hi@scottruzal.com';  //Recipient's E-mail
 $emailTo = $_REQUEST['email'];
 
 $name = $_REQUEST['name'];
