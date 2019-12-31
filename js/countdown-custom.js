039b8ba302e0422d08cdf6915d4aa282ce108266
ch0wdan <jordandegeus@gmail.com>
2019-12-31 10:31:43 -0800

    fixing count down timer to match proper datae

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2020, 01-01, 01, 10)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2020, 01-01, 18, 11)}); // year, month, date, hour
         });
 });
